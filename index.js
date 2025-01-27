54e0ef0922b02c4dfd77e4c05fc5a46f4468221b
Hoppy <dev.hoppy@gmail.com>
2025-01-27 12:27:46 +0100

    move frontend to static folder

--- index.js
+++ index.js
@@ -118,10 +118,11 @@ app.delete("/:folder/:file", authenticate, (req, res) => {
     res.json({ message: "File deleted successfully" });
 });
 
+app.use('/:folder', express.static("public"));
+
 app.use((req, res, next) => {
     res.status(404).json({ error: "Not found" });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
